70c6573bd0c90636bbc61038afc45b75dc844e91
mendesbayout <mendesbayout@gmail.com>
2020-06-30 12:34:12 -0300

    second commit

--- myApp.js
+++ myApp.js
@@ -1,6 +1,21 @@
-/** # MONGOOSE SETUP #
-/*  ================== */
+const express = require('express');
+const app = express();
+
 
 /** 1) Install & Set up mongoose */
-const mongoose = require('mongoose');
-mongoose.connect(process.env.MONGO_URI);
+require('dotenv').config({ path: '.env' });
+
+
+const MongoClient = require('mongodb').MongoClient;
+const uri = "mongodb+srv://<joao>:<1234>@cluster0.dwiyl.mongodb.net/<cluster0>?retryWrites=true&w=majority";
+const client = new MongoClient(uri, { useNewUrlParser: true, useUnifiedTopology: true  });
+client.connect(err => {
+  const collection = client.db("test").collection("devices");
+  // perform actions on the collection object
+  console.log("Connected to db")
+});
+
+const PORT = process.env.PORT || 8000;
+
+app.listen(PORT, console.log(`server started on port ${PORT}`));
+
