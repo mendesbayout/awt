f6eb742af434d5642779f80329dc2875c15d70ff
mendesbayout <mendesbayout@gmail.com>
2020-07-01 10:29:47 -0300

    Master update

--- myApp.js
+++ myApp.js
@@ -10,7 +10,20 @@ const mongoose = require('mongoose');
 mongoose.connect(process.env.MONGO_URI, { useNewUrlParser: true, useUnifiedTopology: true }, () => {console.log("Connected to Database")})
 .catch(err => console.log(err));
 
+var Schema = mongoose.Schema;
 
+var blogSchema = new Schema({
+  title:  String, // String is shorthand for {type: String}
+  author: String,
+  body:   String,
+  comments: [{ body: String, date: Date }],
+  date: { type: Date, default: Date.now },
+  hidden: Boolean,
+  meta: {
+    votes: Number,
+    favs:  Number
+  }
+});
 
 const PORT = process.env.PORT || 8000;
 
